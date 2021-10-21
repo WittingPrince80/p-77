3c05d252bb54e4e4659073695d99c93baacf9121
WittingPrince80 <87693035+WittingPrince80@users.noreply.github.com>
2021-10-21 17:05:59 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var f_name = "Minecraft ";
+var l_name = "Dungons";
+var a = 10;
+var b = 10;
+   var ad = f_name.concat(l_name)
+          console.log(ad)
+          var add = a + b
+          console.log(add)
+
+
